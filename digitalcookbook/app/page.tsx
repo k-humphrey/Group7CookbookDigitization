918d6ba504e10df8706936a3e6d9b81fcec2d984
Zack Enright <zenright08@gmail.com>
2025-11-19 15:04:16 -0600

    Init commit page.tsx

--- digitalcookbook/app/page.tsx
+++ digitalcookbook/app/page.tsx
@@ -1,65 +1,67 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main className="min-h-screen flex flex-col items-center justify-start bg-base-100">
+
+      {/* Background picture */}
+      <div 
+        className="w-full bg-cover bg-center bg-no-repeat py-30 flex flex-col items-center"  
+        style={{
+          backgroundSize: "110%", 
+          backgroundImage: "url('/searchbackground.jpg')" 
+          }}
+      >
+      
+        {/* Logo + Subtitle */}
+        <div className="text-center mb-5">
+          <h1 className="text-6xl md:text-[5rem] font-black text-slate-950 leading-none">
+            Thrifty Bites
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+
+          
+          <p className="mt-5 pt-5 text-base max-w-3xl md:text-2xl text-slate-950">
+            Search by ingredients you have or find the perfect recipe
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        {/* Selected Ingredients Bar*/}
+        <div className="w-full max-w-3xl mb-2">
+          <div className="rounded-3xl bg-[#DEE4D6] px-4 py-3 flex items-center gap-4">
+            <div className="flex-1">
+              <p className="text-xs font-semibold uppercase text-slate-950">
+                Selected Ingredients:
+              </p>
+
+              {/* Placeholder for Ingredients*/}
+              <div className="mt-2 flex flex-wrap gap-2 text-slate-950">
+                (Your selected ingredients will appear here)
+              </div>
+            </div>
+
+            <button className="btn btn-warning btn-sm md:btn-md shrink-0 text-slate-950">
+              Search Recipes
+            </button>
+          </div>
         </div>
-      </main>
-    </div>
+
+        {/* Search Bar */}
+        <div className="w-full max-w-3xl">
+          <label className="input input-bordered rounded-full w-full flex items-center gap-3 h-12 px-6 bg-[#DEE4D6]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5 opacity-70"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              strokeWidth="2"
+            >
+              <circle cx="11" cy="11" r="7" />
+              <line x1="16.65" y1="16.65" x2="21" y2="21" />
+            </svg>
+
+            <input type="text" className="grow placeholder:text-slate-950 bg-transparent" placeholder="Search recipes or ingredients..." />
+          </label>
+        </div>
+      </div>  
+    </main>
   );
 }
