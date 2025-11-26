4adf242bebff37d95827c4e5f700872ca01fe8a3
Justin Dieter <jdieter42@tntech.edu>
2025-11-25 20:39:48 -0600

    Updated search/GET return all information about all recipes

--- digitalcookbook/app/api/search/route.ts
+++ digitalcookbook/app/api/search/route.ts
@@ -1,16 +1,11 @@
 import { connectToDB } from "@/lib/connectToDB";
-import Recipe from '../../../models/models/Recipe';
+import Recipe from "@/models/models/Recipe";
+import Ingredient from "@/models/models/Ingredient";
+import Appliance from "@/models/models/Appliance";
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request){
     await connectToDB();
-    const recipies = await Recipe.find({});
+    const recipies = await Recipe.find({}).populate("ingredients.ingredient").populate("appliances");
     return NextResponse.json(recipies);
-}
-
-export async function POST(req: Request){
-    const body = await req.json();
-    await connectToDB;
-    const recipe = await Recipe.create(body);
-    return NextResponse.json(recipe);
 }
