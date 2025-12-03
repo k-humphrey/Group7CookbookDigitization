2c04cfdab486b614f15f55648c3d8c75f17b8202
Justin Dieter <jdieter42@tntech.edu>
2025-12-02 18:06:58 -0600

    Added test for api/recipes/byTitle/route

---
+++ digitalcookbook/tests/api/testByTitle.ts
@@ -0,0 +1,15 @@
+// Test: api/recipes/byTitle/route
+
+export async function testByTitle() {
+    try {
+        // title to search by
+        const searchTerm = "Hawaiian Chicken with Steamed Broccoli"
+        // get searched for recipes and display them if able
+        const recipes = await (await fetch(`http://localhost:3000/api/recipes/byTitle?search=${searchTerm}`)).json();
+        console.log(`Recipe by title ${searchTerm}:\n`, recipes);
+    } catch (err) {
+        console.error("Error testing api/recipes/byTitle/route: ", err);
+    }
+}
+
+testByTitle();
