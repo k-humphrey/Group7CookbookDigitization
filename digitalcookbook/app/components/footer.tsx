bb1f6ad516378bee96f6b4e4fe626cb1ddfa0c89
John Yaw <jsyaw42@gmail.com>
2025-12-05 12:27:08 -0600

    Routed footer links

--- digitalcookbook/app/components/footer.tsx
+++ digitalcookbook/app/components/footer.tsx
@@ -11,10 +11,29 @@ export default function Footer(){
         {/* Quick Links */}
         <nav>
             <h6 className="footer-title">Quick Links</h6>
-            <a className="link link-hover">Recipes</a>
-            <Link href = "/tools" className="link link-hover">Tools</Link>
-            <a className="link link-hover">Community</a>
-            <a className="link link-hover">Shopping List</a>
+            
+                <Link
+                    href="/all-recipes"
+                    rel="noopener noreferrer"
+                    className="link link-hover" >
+                    All Recipes
+                </Link>
+    
+                <Link
+                    href="/all-recipes"
+                    rel="noopener noreferrer"
+                    className="link link-hover" >
+                    Tools
+                </Link>
+         
+
+                <Link
+                    href="/all-recipes"
+                    rel="noopener noreferrer"
+                    className="link link-hover" >
+                    Shopping List
+                </Link>
+
         </nav>
 
         {/* Resources */}
@@ -91,5 +110,11 @@ export default function Footer(){
         </footer>
 
         </>
+
+
     );
+
+
+
 }
+
