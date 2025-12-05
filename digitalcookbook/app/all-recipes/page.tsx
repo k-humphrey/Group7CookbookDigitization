c4aaad5135b3dd8b57697941d9ce83fc9bd02c04
Lela <Lagracy42@tntech.edu>
2025-12-05 11:41:42 -0600

    added internal pages for navbar

---
+++ digitalcookbook/app/all-recipes/page.tsx
@@ -0,0 +1,9 @@
+//app/all-recipes/page.tsx
+
+export default function AllRecipesPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">All Recipes</h1>
+    </main>
+  );
+}
---
+++ digitalcookbook/app/emergency-preparedness/page.tsx
@@ -0,0 +1,9 @@
+//app/emergency-preparedness/page.tsx
+
+export default function EmergencyPreparednessPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Emergency Preparedness</h1>
+    </main>
+  );
+}
