60ddf6e06f4644d1ad3d22e3a4b6c71c36b1b343
John Yaw <jsyaw42@gmail.com>
2025-12-05 09:03:53 -0600

    added kitchen tool page

---
+++ digitalcookbook/app/tools/page.tsx
@@ -0,0 +1,24 @@
+
+//app/communityresources/page.tsx
+
+
+
+export default function KitckenTool() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-start">
+
+      <div 
+        className="w-full bg-cover bg-center bg-repeat py-30 flex flex-col items-center"  
+        style={{
+          backgroundSize: "110%", 
+          backgroundImage: "bg-white"
+          }}/>  
+      <div>
+        <h6 className="text-6xl md:text-[5rem] font-black text-slate-950 leading-none">Kitcken Tools </h6>
+        
+      </div>
+      <div/>
+
+    </main>
+  );
+}
