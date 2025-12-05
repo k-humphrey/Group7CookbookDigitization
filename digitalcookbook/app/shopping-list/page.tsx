03f847cd5aced177be04f4207fe2e7eb84b7d7e1
John Yaw <jsyaw42@gmail.com>
2025-12-05 13:41:58 -0600

    added shopping list

---
+++ digitalcookbook/app/shopping-list/page.tsx
@@ -0,0 +1,20 @@
+
+export default function KitckenTool() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-start">
+
+      <div 
+        className="w-full bg-cover bg-center bg-repeat py-30 flex flex-col items-center"  
+        style={{
+          backgroundSize: "110%", 
+          backgroundImage: "bg-white"
+          }}/>  
+      <div>
+        <h6 className="text-6xl md:text-[5rem] font-black text-slate-950 leading-none">Shopping Lists </h6>
+        
+      </div>
+      <div/>
+
+    </main>
+  );
+}
