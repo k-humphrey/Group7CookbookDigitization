7623fb63a64c4439196c1fde66a1a616b13edf76
Lela <Lagracy42@tntech.edu>
2026-01-28 17:05:04 -0600

    deletion of extra files

---
+++ digitalcookbook/app/measurement-converter/page.tsx
@@ -0,0 +1,24 @@
+
+//app/measurement-converter/page.tsx
+
+
+
+export default function MeasurementConverter() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-start">
+
+      <div 
+        className="w-full bg-cover bg-center bg-repeat py-30 flex flex-col items-center"  
+        style={{
+          backgroundSize: "110%", 
+          backgroundImage: "bg-white"
+          }}/>  
+      <div>
+        <h6 className="text-6xl md:text-[5rem] font-black text-slate-950 leading-none">Measurement Converter </h6>
+        
+      </div>
+      <div/>
+
+    </main>
+  );
+}
