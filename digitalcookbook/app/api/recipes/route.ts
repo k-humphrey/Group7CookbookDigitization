8419200f4c741952030723761468654855d0b4cf
Justin Dieter <jdieter42@tntech.edu>
2026-02-09 20:41:22 -0600

    Added page and limit parameters

--- digitalcookbook/app/api/recipes/route.ts
+++ digitalcookbook/app/api/recipes/route.ts
@@ -6,8 +6,25 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request){
     await connectToDB();
-    const recipies = await Recipe.find({})
-    .populate({ path: "appliances", model: Appliance })
-    .populate({ path: "ingredients.ingredient", model: Ingredient });
-    return NextResponse.json(recipies);
+
+    // Get page info
+    const url = new URL(req.url);
+    const pageInfo = {pageNumber: Number(url.searchParams.get("page") || 1), pageLimit: Number(url.searchParams.get("limit") || 20)};
+
+    let recipes;
+    if(pageInfo.pageLimit) { // if limit return recipes limited
+        recipes = await Recipe.find({})
+        .skip((pageInfo.pageNumber - 1) * pageInfo.pageLimit)
+        .limit(pageInfo.pageLimit)
+        .populate({ path: "appliances", model: Appliance })
+        .populate({ path: "ingredients.ingredient", model: Ingredient });
+
+    } else { // else return all recipes
+        recipes = await Recipe.find({})
+        .populate({ path: "appliances", model: Appliance })
+        .populate({ path: "ingredients.ingredient", model: Ingredient });
+
+    }
+
+    return NextResponse.json(recipes);
 }
