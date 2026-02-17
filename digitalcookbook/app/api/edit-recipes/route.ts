b7a4a26365be24fbc418bc5ffa285e3db8a7726b
Kayla Humphrey <humphreykayla905@gmail.com>
2026-02-17 12:53:35 -0600

    post for adding new recipes

---
+++ digitalcookbook/app/api/edit-recipes/route.ts
@@ -0,0 +1,42 @@
+import { cookies } from "next/headers";
+import { isAdminAuthenticated } from "@/lib/checkAdminAuth";
+import { NextResponse } from "next/server";
+import { connectToDB } from "@/lib/connectToDB";
+import Recipe from "@/models/Recipe";
+
+//posting, adding new recipes
+export async function POST(req: Request) {
+  const cookieStore = await cookies(); 
+
+  if (!isAdminAuthenticated(cookieStore)) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try{
+    await connectToDB(cookieStore);
+
+    const data = await req.json();
+
+    const recipe = await Recipe.create(data);
+
+    return NextResponse.json(
+      { success: true, recipe },
+      { status: 201 }
+    );
+  } catch(err: any){
+    console.error("POST /api/edit-recipes error:", err);
+    return NextResponse.json(
+      { error: err.message },
+      { status: 500 }
+    );
+  }
+}
+
+//updating, update existing recipes
+export async function PUT(req: Request) {
+    const cookieStore = await cookies(); 
+
+  if (!isAdminAuthenticated(cookieStore)) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  return NextResponse.json({ success: true });
+}
