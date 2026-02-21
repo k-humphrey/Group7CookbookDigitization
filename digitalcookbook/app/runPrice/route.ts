8831476a4b3c9d8d6ae636b7ca6794ed0a3bf734
Kayla Humphrey <humphreykayla905@gmail.com>
2026-02-21 17:43:34 -0600

    price puller wont work rn

--- digitalcookbook/app/runPrice/route.ts
+++
@@ -1,6 +0,0 @@
-import { runPricePuller } from "@/lib/pricePuller";
-
-export async function GET() {
-    const result = await runPricePuller();
-    return Response.json(result);
-}
--- digitalcookbook/lib/scheduler.ts
+++
@@ -1,7 +0,0 @@
-import cron from 'node-cron';
-import { runPricePuller } from './pricePuller';
-
-cron.schedule('0 3 1 * *', async () => {
-    console.log("Running monthly price puller...");
-    await runPricePuller();
-});
