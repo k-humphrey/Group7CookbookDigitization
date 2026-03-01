95b2539f191b9bd78bc699aa9e2d406624c37830
John Yaw <jsyaw42@gmail.com>
2026-02-28 19:49:45 -0600

    added language strings

---
+++ digitalcookbook/app/shopping-list/shoppingListStrings.ts
@@ -0,0 +1,22 @@
+export const SHOPPING_LIST_STRINGS = {
+  en: {
+    title: "Shopping List",
+    clearAll: "Clear All",
+    remove: "Remove",
+    servings: "Servings:",
+    seeMore: "See More",
+    seeLess: "See Less",
+    more: "more",
+    totalCost: "Total Cost",
+  },
+  es: {
+    title: "Lista de Compras",
+    clearAll: "Borrar Todo",
+    remove: "Eliminar",
+    servings: "Porciones:",
+    seeMore: "Ver Más",
+    seeLess: "Ver Menos",
+    more: "más",
+    totalCost: "Costo Total",
+  },
+};
