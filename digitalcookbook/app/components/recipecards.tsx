e08d04f0391e610f62e82e9462308450cdab0c09
John Yaw <jsyaw42@gmail.com>
2026-02-28 19:45:02 -0600

    changed the serving size for storage

--- digitalcookbook/app/components/recipecards.tsx
+++ digitalcookbook/app/components/recipecards.tsx
@@ -5,36 +5,39 @@ import Image from "next/image";
 import { useLang } from "@/app/components/languageprovider";
 import { useState } from "react";
 import Toast from "@/app/components/toast";
+import { Recipe, SelectedRecipe } from "@/app/meal-planner/page";
 
 interface Props {
-  recipes: any[];
+  recipes: Recipe[];
 }
 
 const STRINGS = {
   en: {
-    viewRecipes: "View Recipe"
+    viewRecipes: "View Recipe",
+    saveRecipe: "Save Recipe",
+    addToShopping: "Add to Shopping"
   },
   es: {
-    viewRecipes: "Ver receta"
+    viewRecipes: "Ver receta",
+    saveRecipe: "Guardar receta",
+    addToShopping: "Agregar a compras"
   }
 };
+
 export default function RecipeGrid({ recipes }: Props) {
 
-    const [toastMessage, setToastMessage] = useState("");
-    const langContext = useLang();
-    const lang = langContext?.lang ?? 'en';
-    const t = STRINGS[lang];
+  const [toastMessage, setToastMessage] = useState("");
+  const langContext = useLang();
+  const lang = langContext?.lang ?? 'en';
+  const t = STRINGS[lang];
 
-
-  try {
-    return (
+  return (
     <div>
       <div className="mt-6 mr-3 grid gap-3 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-        {recipes.map((recipe: any) => (
+        {recipes.map((recipe) => (
           <Link key={recipe._id} href={`/single-recipe/${recipe._id}`} className="block">
             <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow flex flex-col">
               
-              {/* Image */}
               <figure className="h-48 overflow-hidden bg-base-200">
                 {recipe.imageURI ? (
                   <Image
@@ -52,64 +55,74 @@ export default function RecipeGrid({ recipes }: Props) {
                 )}
               </figure>
 
-              {/* Content */}
               <div className="card-body flex-1 flex flex-col -m-1">
                 <h2 className="card-title text-lg line-clamp-2 min-h-14">
                   {recipe.title?.[lang]}
                 </h2>
 
-                {/* Tags */}
-                <div className="mt-1 flex flex-wrap gap-2 min-h-7">
-                  {(() => {const tagObj = lang === "es" ? (recipe.espTags ?? {}) : (recipe.tags ?? {});
-                    return Object.entries(tagObj).filter(([_, value]) => value === true).map(([tag]) => (
-                        <div key={tag} className={`badge ${(tag === "Blue Ribbon" || tag === "Cinta Azul") ? "badge-info" : "badge-success"}`}>
-                          {tag}
-                        </div>
-                      ));
-                  })()}
-                </div>
-
                 <div className="card-actions justify-end mt-2 flex gap-2">
-                    {/* View Recipe Button */}
-                    <div className="btn btn-sm btn-success">{t.viewRecipes}</div>
 
-                    {/* Add to List Button */}
-                    <button
-                      className="btn btn-sm btn-primary"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        // prevent navigating to recipe page
-                        e.stopPropagation(); 
-                        //looks for shooping list in browser
-                        const saved = localStorage.getItem("shoppingList");
-                        const list = saved ? JSON.parse(saved) : [];
-                        //checks if the recipe is already on the list
-                        if (!list.find((item: any) => item._id === recipe._id)) {
-                          //adds the recipe if its not already on the list
-                          list.push({
-                            _id: recipe._id,
-                            title: recipe.title?.[lang],
-                            imageURI: recipe.imageURI,
-                          });
-                          //saves updated list back to browser
-                          localStorage.setItem("shoppingList", JSON.stringify(list));
-                          //message
-                          setToastMessage(`${recipe.title?.[lang]} added to Shopping List`);
-                        }
-                      }}
-                    >
-                      Add to List
-                    </button>
+                  <div className="btn btn-sm btn-success">
+                    {t.viewRecipes}
                   </div>
+
+                  {/* SAVE RECIPE */}
+                  <button
+                    className="btn btn-sm btn-primary"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+
+                      const saved = localStorage.getItem("savedRecipe");
+                      const list = saved ? JSON.parse(saved) : [];
+
+                      if (!list.find((item: any) => item._id === recipe._id)) {
+                        list.push({
+                          _id: recipe._id,
+                          title: recipe.title,
+                          imageURI: recipe.imageURI,
+                        });
+
+                        localStorage.setItem("savedRecipe", JSON.stringify(list));
+                        setToastMessage(`${recipe.title?.[lang]} added to Saved Recipe`);
+                      }
+                    }}
+                  >
+                    {t.saveRecipe}
+                  </button>
+
+                  {/* ADD TO SHOPPING */}
+                  <button
+                    className="btn btn-sm btn-secondary"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+
+                      const saved = localStorage.getItem("shoppingList");
+                      const list: SelectedRecipe[] = saved ? JSON.parse(saved) : [];
+
+                      if (!list.find((item) => item.recipe._id === recipe._id)) {
+                        list.push({
+                          recipe: recipe,
+                          servings: 4
+                        });
+
+                        localStorage.setItem("shoppingList", JSON.stringify(list));
+                        setToastMessage(`${recipe.title?.[lang]} added to Shopping List`);
+                      }
+                    }}
+                  >
+                    {t.addToShopping}
+                  </button>
+
+                </div>
               </div>
             </div>
           </Link>
         ))}
       </div>
-      <Toast message={toastMessage} onClose={()=> setToastMessage("")}/>
-      </div>
-    );
-  } catch {
-    return <p className="mt-6 text-sm text-slate-600" >No recipes found.</p>;
-  }
+
+      <Toast message={toastMessage} onClose={() => setToastMessage("")} />
+    </div>
+  );
 }
