43522353d3aac82ae56db19af38ed64f10668021
MaxwellAskren <Maaskren42@tntech.edu>
2026-03-03 15:10:31 -0600

    Removed "use client" from editRecipes file

--- digitalcookbook/app/admin/editRecipes/page.tsx
+++ digitalcookbook/app/admin/editRecipes/page.tsx
@@ -1,8 +1,6 @@
 //app/admin/editRecipes/page.tsx
 //Page for admin to view, edit, add, and delete recipes once logged in via the admin portal  
 
-"use client";
-
 import { connectToDB } from "@/lib/connectToDB";
 import Recipe from "@/models/Recipe";
 import { cookies } from "next/headers";
