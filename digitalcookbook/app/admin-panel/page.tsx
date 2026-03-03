a3cc37e6806a1c6a1449aa73e8259bb99acbb301
MaxwellAskren <Maaskren42@tntech.edu>
2026-03-03 15:35:19 -0600

    Removed client imports from adminPanel

--- digitalcookbook/app/admin-panel/page.tsx
+++ digitalcookbook/app/admin-panel/page.tsx
@@ -4,15 +4,11 @@
 import { cookies } from "next/headers";
 import { isAdminAuthenticated } from "@/lib/checkAdminAuth";
 import AdminLoginPage from "../admin-login/page";
-import { useState } from "react";
 import AdminNavBar from "../components/adminNavBar";
 
 export default async function AdminPanelPage() {
 	//get cookies
 	const cookieStore = await cookies(); 
-
-	//Variable to keep track of which value from the dropdown is selected
-	const [selected, setSelected] = useState("");
 
 	//check authentication
 	if (!isAdminAuthenticated(cookieStore)) {
