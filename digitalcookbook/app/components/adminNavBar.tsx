71b8729345f6f68a8f0ee67c919786f88e740ff0
MaxwellAskren <Maaskren42@tntech.edu>
2026-03-03 15:08:28 -0600

    Pushed AdminNavBar and changes to file structure and AdminPanel page
    Added a new component called AdminNavBar to navigate through the new admin pages I added. I also moved the AdminPanel page to the Admin/editRecipes page.

---
+++ digitalcookbook/app/components/adminNavBar.tsx
@@ -0,0 +1,47 @@
+//app/components/adminNavBar.tsx
+
+"use client";
+import Link from "next/link";
+
+export default function AdminNavBar(){
+    return (
+        <header className="">
+            <nav>
+                <summary>Edit/Add Website Data</summary>
+                <Link
+                    href="/app/admin/EditResources"
+                    className="hover:underline" >
+                    Edit/Add Community Resources
+                </Link>
+                <Link 
+                    href="/app/admin/EditPartners"
+                    target="_blank"
+                    rel="noopener noreferrer" 
+                    className="hover:underline" >
+                    Edit/Add Community Partners
+                </Link>
+                <Link 
+                    href="/app/admin/EditAdvertisements"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline" >
+                    Edit/Add Advertisements
+                </Link>
+                <Link 
+                    href="/app/admin/EditRecipes"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline" >
+                    Edit/Add Recipes
+                </Link>
+                <Link
+                    href="/app/admin/Logout"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline" >
+                    Logout
+                </Link>
+            </nav>
+        </header>
+    )
+}
