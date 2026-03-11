156585494d48f11321b2c8d9e870721162ea32e1
Kayla Humphrey <humphreykayla905@gmail.com>
2026-03-11 16:26:30 -0500

    added sizes to infocard

--- digitalcookbook/app/components/infocard.tsx
+++ digitalcookbook/app/components/infocard.tsx
@@ -40,6 +40,7 @@ export default function InfoCard(
 						alt={imageAlt || title}
 						className="object-cover"
 						fill
+						sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
 					/>
 				</figure>
 			)}
