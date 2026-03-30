ffd5061f805c81f0e338c1479cf50efbce53957d
Kayla Humphrey <humphreykayla905@gmail.com>
2026-03-30 13:00:05 -0500

    typescript fix google maps

---
+++ digitalcookbook/lib/google-maps.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "gmp-map": any;
+    "gmp-advanced-marker": any;
+    "gmp-place-picker": any;
+    "gmp-place-directions": any;
+  }
+}
