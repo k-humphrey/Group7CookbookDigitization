0b39c32eae50389cf251dd1a771987eb545778fe
Kayla Humphrey <humphreykayla905@gmail.com>
2026-04-07 15:13:53 -0500

    fix

--- digitalcookbook/app/components/adminPanelClient.tsx
+++ digitalcookbook/app/components/adminPanelClient.tsx
@@ -65,7 +65,7 @@ export default function AdminPanelClient({ recipes }: { recipes: any[] }) {
 
   	return (
     <>
-	<div className="flex justify-between items-center mx-2">
+	<div className="flex justify-center mx-2">
 		<Link href="/comm-partners" className="block px-4 py-2 hover:bg-gray-100">LINK</Link>
 		<Link href="/comm-partners" className="block px-4 py-2 hover:bg-gray-100">LINK</Link>
 		<Link href="/comm-partners" className="block px-4 py-2 hover:bg-gray-100">LINK</Link>
