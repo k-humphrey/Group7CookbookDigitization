3e6244f01a3ac0efcac2ab5e18a75ce34e7e53a0
Kayla Humphrey <humphreykayla905@gmail.com>
2026-04-29 17:53:04 -0500

    naming

--- digitalcookbook/app/components/submittedRecipeSelector.tsx
+++ digitalcookbook/app/components/submittedRecipeSelector.tsx
@@ -46,7 +46,7 @@ export default function SubmittedRecipeSelector() {
         if(modalRecipe.status == "rejected"){
             console.log("Deleting recipe with id\n")
             console.log(updated._id)
-            await fetch("/api/edit-recipes/", {
+            await fetch("/api/submitted-recipes/", {
             method: "DELETE",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({_id: updated._id}), 
